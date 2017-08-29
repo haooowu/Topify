f44338dd40bc1d5e67587aa057f5a1de60f4b191
Dana Manoli <dmanoli1@gmail.com>
2017-08-29 12:43:55 -0400

    inital commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 //a task to compile our sass
 // gulp.task('bacon',function(){
 // 	console.log('bbbbb');
 // });
 gulp.task('styles', () => {
-	//single ling no return..
 	return gulp.src('./dev/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('./public/styles/'))
-		//dest?
+		.pipe(gulp.dest('./public/styles'))
+		.pipe(reload({stream: true}));
 });
 
 //task that complie js
@@ -22,13 +24,20 @@ gulp.task('scripts', () => {
 			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('./public/scripts'))
+		.pipe(reload({stream: true}));
 });
 
 //a task to watch all of my other tasks
 gulp.task('watch', function() {
 	gulp.watch('./dev/scripts/*.js', ['scripts']);
 	gulp.watch('./dev/styles/*.scss', ['styles']);
-	// gulp.watch('*.html', reload);
+	gulp.watch('*.html', reload);
 });
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('browser-sync', () => {
+  browserSync.init({
+    server: '.'
+  })
+});
+
+gulp.task('default', ['browser-sync','styles', 'scripts', 'watch']);
