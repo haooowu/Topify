0df5e421e6c4c2a2acbd72f18091bfcbbcc8bd37
Hao Wu <wh1993123@hotmail.com>
2017-09-03 03:59:37 -0400

    pre responsive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -3,11 +3,20 @@ const canadaTop = "37i9dQZEVXbKj23U1GF4IR";
 const canadaViral = "37i9dQZEVXbKfIuOAZrk7G";
 const globalTop = "37i9dQZEVXbMDoHDwVN2tF";
 const globalViral = "37i9dQZEVXbLiRSasKsNU9";
-//TODO: dynamically change option
-//TODO: ALBUM/#
+// const spotifyFont = `<i class="fa fa-spotify" aria-hidden="true"></i>`
 spotifyApp.albumInfo = {};
 let headers;
+let defaultList = canadaTop;
 //init
+spotifyApp.init = function(){
+	spotifyApp.tokenRequest();
+	spotifyApp.playListListener();
+	$(".main__holder").css("display","none");
+	$("#splashText").css("display","none");
+	$("#splashText").text(`Canada Top 50`)
+	$("#splashText").fadeIn(1000);
+}
+
 spotifyApp.tokenRequest = function(){
 	$.ajax({//proxy
 		url: 'http://proxy.hackeryou.com',
@@ -27,13 +36,13 @@ spotifyApp.tokenRequest = function(){
 		})
 	}).then((data) => {
 		headers = {'Authorization': `${data.token_type} ${data.access_token}`}
-		spotifyApp.chartsPlaylist();
+		spotifyApp.chartsPlaylist(defaultList);
 	});
 }//end of tokenRequest
 
-spotifyApp.chartsPlaylist = function(){// geting toplist by category
+spotifyApp.chartsPlaylist = function(playlist){// geting toplist by category
 	spotifyApp.topFifty = $.ajax({//https://developer.spotify.com/web-api/console/get-playlist/
-		url:`https://api.spotify.com/v1/users/spotifycharts/playlists/${canadaTop}`,
+		url:`https://api.spotify.com/v1/users/spotifycharts/playlists/${playlist}`,
 		method: 'GET',
 		dataType: 'json',
 		headers,
@@ -76,85 +85,101 @@ spotifyApp.chartsPlaylist = function(){// geting toplist by category
 spotifyApp.galleryListener = function(){
 	let selectedAlbum;
 	$(".imgHolder").click(function(e){
+		$("#splashText").css("display","none");
+		$(".main__holder").fadeIn();
 		let contentId = $(this).attr("id");//targeting DOM ID
 		selectedAlbum = spotifyApp.albumInfo[contentId];
 		$("#splash").empty();//get hero img
 		$("#splash").append(`<img src = ${selectedAlbum.imageSrcSplash}>`)
 		console.log((spotifyApp.albumInfo[contentId]))
 		//ajax for get album info
-		spotifyApp.getAlbumtById(contentId);
+		spotifyApp.getAlbumtById(contentId,selectedAlbum.redirectLink);
 		$("#info").empty();
-		$("#info").append(`<h2 id="artist__name"></h2>`);
+		$("#info").append(`<div class="track__name"><h2>${selectedAlbum.trackName}</h2></div>`)
+		$("#info").append(`<h3 id="artist__name"></h3>`);
 		for (let i = Object.keys(selectedAlbum.artists).length - 1; i>=0; i--){
 			$("#artist__name").text(`${Object.keys(selectedAlbum.artists)}`)
 		}
-		//add second/third/fourth... artist 
-		$("#info").append(`<div class="album__name"><h4>Album: ${selectedAlbum.albumName}</h4></div>`)
-		$("#info").append(`<div class="track__name"><h4>Track: ${selectedAlbum.trackName}</div></h4>`)
 		$("#info").append(`
-			<div class="button__set-1">
-				<span>${selectedAlbum.duration}</span>
-				<span>${selectedAlbum.popularity}</span>
+			<div class="button__set">
+				<span><i class="fa fa-clock-o" aria-hidden="true"></i>${selectedAlbum.duration}</span>
+				<span><i class="fa fa-star" aria-hidden="true"></i>${selectedAlbum.popularity}</span>
+				<span>${selectedAlbum.albumType}</span>
 			</div>`)
-    
+		$("#info").append(`<div class="album__name"><h4>Album: ${selectedAlbum.albumName}</h4></div>`)
 		if (selectedAlbum.explicit === true) {
-			$("#info").append(`
-				<div class="button__set-2">
-					<span>${selectedAlbum.albumType}</span>
-					<span id="censorship">explicit</span>
-				</div>`)
-		} else { 
-			$("#info").append(`
-				<div class="button__set-2">
-					<span>${selectedAlbum.albumType}</span>
-				</div>`)
-
-		}
-		// $("#info").append(`<span>${selectedAlbum.popularity}</span>`)
-		// $("#info").append(`<span>${selectedAlbum.albumType}</span>`)
-		// $("#info").append(`<span>${selectedAlbum.explicit}</span>`)
-    
-		$("#info").append(`<a href ="${selectedAlbum.redirectLink}"></a>`)
+			$(".button__set").append(`<span id="censorship">explicit</span>`)
+		} 
 	});
 }
 
-//genre always emtpy...
-spotifyApp.getAlbumtById = function(album){
+spotifyApp.getAlbumtById = function(album, linkUrl){
 	$.ajax({//https://developer.spotify.com/web-api/console/get-track/?id=3n3Ppam7vgaVa1iaRUc9Lp
 		url:`https://api.spotify.com/v1/albums/${album}`, //album_id
 		method: 'GET',
 		dataType: 'json',
 		headers,
 	}).then((data) => {
-		console.log("emblum_popularity: " + data.popularity);
-		console.log("release_date: " + data.release_date);
-		console.log("label: " + data.label);
-		console.log("total track: " + data.tracks.items.length);
 		data.tracks.items.forEach(function(element, index) {
 			console.log(index+1 + ". " + element.name);
 		});
-		$("#info").append(`<span>${data.tracks.items.length}</span>`)
+		// $("#info").append(`<span>${data.tracks.items.length}</span>`)
 		$("#info").append(`<p>Released: ${data.release_date}</p>`)
 		$("#info").append(`<p>Record Label: ${data.label}</p>`)
-		//TODO: populate the info correspondingly
+		$("#info").append(`<a href ="${linkUrl}">Listen it on Spotify<i class="fa fa-play-circle" aria-hidden="true"></i></a>`)
 	});
 }
-/*
-	helper functions
-*/
 
-//helper function
+spotifyApp.playListListener = function(){
+	$("#cdnTop").click(function(e){
+		e.preventDefault();
+		spotifyApp.emptyPage();
+		defaultList = canadaTop;
+		$("#splashText").text(`Canada Top 50`)
+		spotifyApp.chartsPlaylist(canadaTop);
+	});
+	$("#globalTop").click(function(e){
+		e.preventDefault();
+		spotifyApp.emptyPage();
+		defaultList = globalTop;
+		$("#splashText").text(`Global Top 50`)
+		spotifyApp.chartsPlaylist(globalTop);
+	});
+	$("#cdnViral").click(function(e){
+		e.preventDefault();
+		spotifyApp.emptyPage();
+		defaultList = canadaViral;
+		$("#splashText").text(`Canada Viral 50`)
+		spotifyApp.chartsPlaylist(canadaViral);
+	});
+	$("#globalViral").click(function(e){
+		e.preventDefault();
+		spotifyApp.emptyPage();
+		defaultList = globalViral;
+		$("#splashText").text(`Global Viral 50`)
+		spotifyApp.chartsPlaylist(globalViral);
+	});
+}
+/* helper functions */
+
 spotifyApp.transformMills = function(millis) {
 	let minutes = Math.floor(millis / 60000);
 	let seconds = ((millis % 60000) / 1000).toFixed(0);
 	return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
-//TODO: helper function, contact string name, separate by comma,click-able with each artists page
-spotifyApp.pushArtists = function(list){
-	//
+spotifyApp.emptyPage = function(){
+	$("#splash").empty();
+	$("#info").empty();
+	$("#gallery").empty();
+	$(".main__holder").css("display","none");
+	$("#splashText").css("display","none");
+	$("#splashText").fadeIn();
 }
 
 $(function(){
-	spotifyApp.tokenRequest();
+	spotifyApp.init();
+	$( "#gallery" ).draggable({
+		axis: "x"
+	});
 });
